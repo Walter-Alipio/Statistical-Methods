ccd66a424f3a82b57ba17b5a1c1c5404c4e4b12a
Walter <silvawalteralipio@gmail.com>
2023-04-17 16:01:09 -0300

    Refactoring test names

--- Estatistica.Test/CentralTendencyTests.cs
+++ Estatistica.Test/CentralTendencyTests.cs
@@ -7,7 +7,7 @@ public class CentralTendencyTests
     [Theory]
     [InlineData(15, new int[] { 10, 18, 11, 15, 20, 21, 16, 10, 8, 20, 16, 22 })]
     [InlineData(31, new int[] { 23, 34, 30, 22, 34, 53, 34, 28, 30, 22 })]
-    public void Mean_ReturnIntValue(int expected, int[] values)
+    public void Mean_ReturnsTheCalculatedMean_FromAnIntArray(int expected, int[] values)
     {
         //Arrange
 
@@ -21,7 +21,7 @@ public class CentralTendencyTests
     [Theory]
     [InlineData(16, new int[] { 10, 18, 11, 15, 20, 21, 16, 10, 8, 20, 16, 22 })]
     [InlineData(30, new int[] { 23, 34, 30, 22, 34, 53, 34, 28, 30, 22 })]
-    public void Median_ReturnIntValue(int expected, int[] values)
+    public void Median_ReturnsTheCalculatedMedian_FromAnIntArray(int expected, int[] values)
     {
         // Given
 
@@ -36,7 +36,7 @@ public class CentralTendencyTests
     [Theory]
     [InlineData(new int[] { 10, 16, 20 }, new int[] { 10, 18, 11, 15, 20, 21, 16, 10, 8, 20, 16, 22 })]
     [InlineData(new int[] { 34 }, new int[] { 23, 34, 30, 22, 34, 53, 34, 28, 30, 22 })]
-    public void Mode_ReturnListInt(int[] expected, int[] values)
+    public void Mode_ReturnsListOfInt_WithCalculatedModeFromAnIntArray(int[] expected, int[] values)
     {
         // Given
 
@@ -67,7 +67,7 @@ public class CentralTendencyTests
 
     [Theory]
     [InlineData(34500, new int[] { 30000, 50000, 25000 }, new int[] { 4, 3, 3 })]
-    public void WeightAverage_ReturnsDoubleValue(double expected, int[] valores, int[] pesos)
+    public void WeightAverage_ReturnsCalculatedResult_ForInputValuesAndWeights(double expected, int[] valores, int[] pesos)
     {
         // Given
 
@@ -82,7 +82,7 @@ public class CentralTendencyTests
     [Theory]
     [InlineData(27, 30, 25)]
     [InlineData(22, 25, 20)]
-    public void Midpoint_ReturnsDoubleValue(double expected, int limiteSup, int limiteInf)
+    public void Midpoint_ReturnsCalculatedMidpoint_GivenSuperiorLimitIntValueAndInferiorLimitIntValue(double expected, int limiteSup, int limiteInf)
     {
         // Given
 
@@ -94,7 +94,7 @@ public class CentralTendencyTests
     }
     [Theory]
     [InlineData(27.5, 30.0, 25.0)]
-    public void MidpointDouble_ReturnsDoubleValue(double expected, double limiteSup, double limiteInf)
+    public void MidpointDouble_ReturnsCalculatedMidpoint_GivenSuperiorLimitDoubleValueAndInferiorLimitDoubleValue(double expected, double limiteSup, double limiteInf)
     {
         // Given
 
