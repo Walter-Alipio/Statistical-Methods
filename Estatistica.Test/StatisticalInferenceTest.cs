b0b6763a2e06b6688cb3caca860c736d9ef2b163
Walter <silvawalteralipio@gmail.com>
2023-04-17 18:06:22 -0300

    Refactoring Test methods names

--- Estatistica.Test/StatisticalInferenceTest.cs
+++ Estatistica.Test/StatisticalInferenceTest.cs
@@ -4,16 +4,16 @@ public class StatisticalInferenceTest
 {
 
     [Fact]
-    public void PearsonCorrelationCoefficient_ReturnsDoubleValue()
+    public void PearsonCorrelationCoefficient_ReturnsCorrelationOfPearsonValue_ForColXValuesAndColYValues()
     {
         // Given
 
-        var x = new int[] { 3, 5, 8, 13, 16, 17, 20, 22 };
-        var y = new int[] { 6, 17, 27, 20, 45, 28, 34, 53 };
+        var colX = new int[] { 3, 5, 8, 13, 16, 17, 20, 22 };
+        var colY = new int[] { 6, 17, 27, 20, 45, 28, 34, 53 };
 
         var expected = 0.84644093213224;
         // When
-        var pearsonCorrelationCoefficient = StatisticalInference.PearsonCorrelationCoefficient(x, y);
+        var pearsonCorrelationCoefficient = StatisticalInference.PearsonCorrelationCoefficient(colX, colY);
         // Then
         Assert.Equal(expected, pearsonCorrelationCoefficient);
     }
@@ -21,12 +21,12 @@ public class StatisticalInferenceTest
     [Theory]
     [InlineData(-0.7745966692414834, new int[] { 1, 2, 3, 4 }, new int[] { 1, 1, 1, 0 })]
     [InlineData(0.9669875568304563, new int[] { 1, 3, 2, 1 }, new int[] { 2, 6, 3, 1 })]
-    public void LinearCorrelationCoefficient_ReturnsDoubleValue(double expected, int[] x, int[] y)
+    public void LinearCorrelationCoefficient_ReturnsCorrelationValue_ForColXValuesAndColYValues(double expected, int[] colX, int[] colY)
     {
         // Given
 
         // When
-        var linearCorrelationCoefficient = StatisticalInference.LinearCorrelationCoefficient(x, y);
+        var linearCorrelationCoefficient = StatisticalInference.LinearCorrelationCoefficient(colX, colY);
 
         // Then
         Assert.Equal(expected, linearCorrelationCoefficient);
@@ -35,7 +35,7 @@ public class StatisticalInferenceTest
     [Theory]
     [InlineData(0.0000567894470833484, 31, 10, 0.75)]
     [InlineData(0.152499399025936, 100, 6, 0.16)]
-    public void BinomialDistribution_ReturnsDecimalValue(decimal expected, int attempts, int target, double success)
+    public void BinomialDistribution_ReturnsDistributionValue_GivenNumberOfAttemptsTargetValueAndSuccessProbability(decimal expected, int attempts, int target, double success)
     {
         // Given
 
